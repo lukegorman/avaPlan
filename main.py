4d7bff11e6875b1a273d919d9712544173b66a3d
luke gorman <lukegorman@gmail.com>
2022-01-04 17:00:59 +0000

    form grid, sliders, button slot/sig

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
+from PyQt5 import QtWidgets, uic
 import sys
-import os
-from PyQt6.QtGui import QGuiApplication
-from PyQt6.QtQml import QQmlApplicationEngine
-from PyQt6.QtQuick import QQuickWindow
-QQuickWindow.setSceneGraphBackend('software')
-app = QGuiApplication(sys.argv)
-engine = QQmlApplicationEngine()
-engine.quit.connect(app.quit)
-engine.load('./UI/main.qml')
-sys.exit(app.exec())
+
+
+class Ui(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Ui, self).__init__()
+        uic.loadUi('UI/plan.ui', self)
+        #self.show()
+
+        self.button = self.findChild(
+            QtWidgets.QPushButton, 'pbEvaluate')  # Find the button
+        # Remember to pass the definition/method, not the return value!
+        self.button.clicked.connect(self.printButtonPressed)
+        
+
+        self.show()
+
+    def printButtonPressed(self):
+        # This is executed when the button is pressed
+        print('printButtonPressed')
+
+
+app = QtWidgets.QApplication(sys.argv)
+window = Ui()
+app.exec_()
+
+
+# QQuickWindow.setSceneGraphBackend('software')
+#app = QGuiApplication(sys.argv)
+#engine = QQmlApplicationEngine()
+# engine.quit.connect(app.quit)
+# engine.load('./UI/main.qml')
+# sys.exit(app.exec())
