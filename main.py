e10af7d7e08574b1e3e3da183f6f2610fc0a5758
luke gorman <lukegorman@gmail.com>
2022-01-05 18:28:39 +0000

    change layout to grid, add state claa

--- main.py
+++ main.py
@@ -1,24 +1,62 @@
-from PyQt5 import QtWidgets, uic
+from PyQt5 import QtGui, QtWidgets, QtCore, uic
 import sys
+
+
+class SateEval:
+        result = 0
+        thirdPartyAssesment = 0 
+        snowFall = 0
+        wind = 0
+        partySize = 0
+        slopeIncline = 0
+        timeOfDay = 0
+        serrac = 0
+
+        def Evaluate(self):
+            self.result = (self.thirdPartyAssesment + self.snowFall +
+                self.wind + self.partySize + self.slopeIncline
+                + self.timeOfDay + self.serrac)
+            return self.result/8
 
 
 class Ui(QtWidgets.QMainWindow):
     def __init__(self):
         super(Ui, self).__init__()
         uic.loadUi('UI/plan.ui', self)
-        #self.show()
+        # self.show()
 
-        self.button = self.findChild(
-            QtWidgets.QPushButton, 'pbEvaluate')  # Find the button
-        # Remember to pass the definition/method, not the return value!
-        self.button.clicked.connect(self.printButtonPressed)
-        
+        self.state = SateEval()
+
+        self.pbEvaluate = self.findChild(
+            QtWidgets.QPushButton, 'pbEvaluate') 
+        self.pbEvaluate.clicked.connect(self.pbEvaluatePressed)
+
+        self.slRisk = self.findChild(
+            QtWidgets.QSlider, 'slRisk')
+        self.slRisk.sliderReleased.connect(self.slRiskReleased)
+
+        #self.slSnow = self.findChild(
+        #    QtWidgets.QSlider, 'slSnow')
+        #self.slSnow.sliderReleased.connect(self.sliderReleased)
+
 
         self.show()
 
-    def printButtonPressed(self):
-        # This is executed when the button is pressed
-        print('printButtonPressed')
+
+    def pbEvaluatePressed(self):
+        print('Risk: ', self.state.Evaluate())
+
+    def slRiskReleased(self):
+        self.state.thirdPartyAssesment = self.slRisk.value()
+        print('printsliderReleased', self.slRisk.value())
+
+
+
+    #def myFunction(self, identifier):
+      #   combo = self.findChild(QtGui.QComboBox,identifier)
+     #    index = combo.currentIndex()
+       #  text = combo.currentText()
+        # data = combo.currentData()
 
 
 app = QtWidgets.QApplication(sys.argv)
@@ -27,8 +65,8 @@ app.exec_()
 
 
 # QQuickWindow.setSceneGraphBackend('software')
-#app = QGuiApplication(sys.argv)
-#engine = QQmlApplicationEngine()
+# app = QGuiApplication(sys.argv)
+# engine = QQmlApplicationEngine()
 # engine.quit.connect(app.quit)
 # engine.load('./UI/main.qml')
 # sys.exit(app.exec())
